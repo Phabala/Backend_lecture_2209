87d5618aee1df4bd36b184dc5e0782ed7e2f1098
Cheolkie Kim <ckiekim@naver.com>
2022-09-21 17:02:57 +0900

    http app

--- 02.http/view/template.js
+++ 02.http/view/template.js
@@ -22,8 +22,8 @@ module.exports = {
         } else {                        // 조회 화면, 생성/수정/삭제 가능
             return `
                 <button onclick="location.href='/create'">생성</button>
-                <button onclick="location.href='/update'">수정</button>
-                <button onclick="location.href='/delete'">삭제</button>
+                <button onclick="location.href='/update?id=${title}'">수정</button>
+                <button onclick="location.href='/delete?id=${title}'">삭제</button>
             `;
         }
     },
@@ -41,6 +41,26 @@ module.exports = {
                     </tr>
                     <tr>
                         <td colspan="2" style="text-align: center;"><input type="submit" value="생성"></td>
+                    </tr>
+                </table>
+            </form>        
+        `;
+    },
+    updateForm: function(title, content) {
+        return `
+            <form action="/update" method="post">
+                <input type="hidden" name="original" value="${title}">
+                <table>
+                    <tr>
+                        <td>제목</td>
+                        <td><input type="text" name="title" value="${title}"></td>
+                    </tr>
+                    <tr>
+                        <td>내용</td>
+                        <td><textarea name="content" cols="60" rows="5">${content}</textarea></td>
+                    </tr>
+                    <tr>
+                        <td colspan="2" style="text-align: center;"><input type="submit" value="수정"></td>
                     </tr>
                 </table>
             </form>        
